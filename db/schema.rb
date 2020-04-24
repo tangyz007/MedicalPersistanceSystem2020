38b30d7918bd342ef34e2423985d0a3eb7946421
Junyu Han <jyhan@tamu.edu>
2020-04-24 17:36:30 -0500

    add fake Requests Navigation bar

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,18 @@ ActiveRecord::Schema.define(version: 20191210075308) do
     t.string   "provider_id"
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.boolean  "admin"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "provider_id"
+  end
+
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
