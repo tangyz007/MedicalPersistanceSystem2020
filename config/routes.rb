28af4d97d5b688f2314bc941a77f091b001c4055
EC2 Default User <ec2-user@ip-172-31-27-105.us-east-2.compute.internal>
2020-04-28 04:06:07 +0000

    signup page function v1

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :skip => [:registrations] 
   resources :questions
+  # resources :signup
   #get '/' => 'questions#login'
   get '/dashboard' => 'questions#dashboard'
-  get '/signup'=> 'provider#signup'
+  # get '/logining'=> 'questions#login'
+  match "provider/signup" => "provider#signup", as: :signup, via: [:get, :post]
+  get '/success'=> 'provider#success'
+  # post '/signup'=> 'provider#signup'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
