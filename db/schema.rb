e534ad7b483b5931114605cd8c4301797726169a
EC2 Default User <ec2-user@ip-172-31-27-105.us-east-2.compute.internal>
2020-04-30 05:06:34 +0000

    update request controller and db schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20191210075308) do
 
   create_table "users", force: :cascade do |t|
     t.boolean  "admin"
+    t.boolean  "provider",               default: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
