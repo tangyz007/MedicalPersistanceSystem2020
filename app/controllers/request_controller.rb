2232304ae3d2d01abcf384be19b5e05922130372
wumrwds <wumrwds@gmail.com>
2020-10-12 16:59:51 +0000

    feat: complete the modification for the register feature

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,6 +1,6 @@
 class RequestController < ApplicationController
     def request_params
-        params.require(:request).permit(:provider_id, :email, :password)
+        params.require(:request).permit(:role, :provider_id, :email, :password)
     end 
     
     def new
@@ -13,6 +13,8 @@ class RequestController < ApplicationController
     
     def create
 
+        puts request_params
+
         if request_params["password"].length < 6 || request_params["password"] == nil
             render html: "<h1>Password can't be blank or less than 6 digits, Please return to previous page to correct! </h1>".html_safe
         elsif request_params["email"] !~ /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -20,8 +22,8 @@ class RequestController < ApplicationController
         elsif Request.find_by(email:request_params["email"]) || User.find_by(email:request_params["email"])
             render html: "<h1>Email has already exist, please wait for approval or use another one. </h1>".html_safe
         else
-            @record = Request.create!(request_params)
-            @users = User.create!(admin:0,provider:0,email:request_params["email"],provider_id:request_params["provider_id"],password:request_params["password"])
+            @record = Request.create!(provider_id:request_params["provider_id"],email:request_params["email"],password:request_params["password"])
+            @users = User.create!(role:request_params["role"],email:request_params["email"],password:request_params["password"])
             redirect_to '/success'
             return 
         end
