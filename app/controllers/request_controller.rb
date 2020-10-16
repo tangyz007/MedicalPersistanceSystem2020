027100e27e0e4277d846aafe737f6c3874048be9
minreng wu <wumrwds@gmail.com>
2020-10-16 02:34:05 +0000

    fix: fix the issue that any two passwords are not matched

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,6 +1,6 @@
 class RequestController < ApplicationController
     def request_params
-        params.require(:request).permit(:role, :provider_id, :email, :password)
+        params.require(:request).permit(:role, :provider_id, :email, :password, :password_check)
     end 
     
     def new
@@ -19,7 +19,7 @@ class RequestController < ApplicationController
             render html: "<h1>Email has already exist, please wait for approval or use another one. </h1>".html_safe
         elsif request_params["password"].length < 6 || request_params["password"] == nil
             render html: "<h1>Password can't be blank or less than 6 digits, Please return to previous page to correct! </h1>".html_safe
-        elsif request_params["password"] != request_params["password_confirm"]
+        elsif !(request_params["password"].eql? request_params["password_check"])
             render html: "<h1>Passwords must match. Please return to previous page to correct it. </h1>".html_safe
         else        
             @record = Request.create!(provider_id:request_params["provider_id"],email:request_params["email"],password:request_params["password"])
