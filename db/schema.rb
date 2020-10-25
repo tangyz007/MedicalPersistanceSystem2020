6fe1ba4c81d4d5b2e680f88573ba82684bf4ceab
Ubuntu <ubuntu@ip-172-31-81-253.ec2.internal>
2020-10-25 05:40:07 +0000

    add patientid attribute to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201012144007) do
+ActiveRecord::Schema.define(version: 20201025040440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20201012144007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "user_id"
+    t.integer  "patient_id"
   end
 
   create_table "requests", force: :cascade do |t|
